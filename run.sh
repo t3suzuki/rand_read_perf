4b53c748f0d239a95430ef4da9ce88ff4d082906
Tomoya Suzuki <t3suzuki@ucsd.edu>
2024-03-13 07:17:31 +0000

    up

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
-for i_th in {1,2,4}
+for i_th in {1,2,4,8}
 do
-    make N_TH=${i_th} URNAD=1
-    sudo ./a.out 2
-    sudo ./a.out 3
-    make N_TH=${i_th} URNAD=0 THETA=0.9
-    sudo ./a.out 2
-    sudo ./a.out 3
-    make N_TH=${i_th} URNAD=0 THETA=0.99
-    sudo ./a.out 2
-    sudo ./a.out 3
+    make N_TH=${i_th} URAND=1
+    sudo ./my 2
+    sudo ./my 3
+    make N_TH=${i_th} URAND=0 THETA=0.7
+    sudo ./my 2
+    sudo ./my 3
+    make N_TH=${i_th} URAND=0 THETA=0.9
+    sudo ./my 2
+    sudo ./my 3
+    make N_TH=${i_th} URAND=0 THETA=0.99
+    sudo ./my 2
+    sudo ./my 3
 done
